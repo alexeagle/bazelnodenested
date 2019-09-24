a295c0bcaf24a8317477f5cc60c814382d0c40b4
Alex Eagle <alexeagle@google.com>
2019-09-23 17:11:57 -0700

    show working thing

--- app-ui/project2/build.js
+++ app-ui/project2/build.js
@@ -1,4 +1,19 @@
 const _ = require("lodash")
+const fs = require("fs")
+const path = require("path")
 const ver = _.VERSION
-console.log('Node Version: ', process.version);
-console.log('Lodash Version: ',ver);
+
+function mkdirp(p) {
+  if (!fs.existsSync(p)) {
+    mkdirp(path.dirname(p));
+    fs.mkdirSync(p);
+  }
+}
+
+const out = process.argv[2];
+mkdirp(path.dirname(out));
+require("fs").writeFileSync(out, `
+console.log('Node Version: ', ${process.version});
+console.log('Lodash Version: ', ${ver});
+`, 'utf-8');
+console.error('wrote', out);
